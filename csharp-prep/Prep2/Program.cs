94f6ec55ba1040cab2758e5ab51df01c9e4b7fb4
JHarvey <har20036@byui.edu>
2023-01-07 22:55:29 -0700

    Prep1 and Prep2 update

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is the grade percentage?");
+        string gradeString = Console.ReadLine();
+        int gradePercent = int.Parse(gradeString);
+        string letter = "z";
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations! you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Oh no! You didn't pass! You'll do better next time!");
+        }
+        Console.WriteLine(letter);
     }
 }
