39a8643961899c0fe53689651e47137d91a265ce
JHarvey <har20036@byui.edu>
2023-02-01 15:36:21 -0700

    p3 update

---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,31 @@
+public class Fraction 
+{
+    private int _top = 0;
+    private int _bottom = 0;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+        public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+        public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+    
+    public string GetFractionString()
+    {
+        string fString = $"{_top}/{_bottom}";
+        return fString;
+    }
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
